79b425ed3e17921c4b7b749f905e301b1bd2a466
SURYANSH ROY <anirudroy.32@gmail.com>
2026-08-13 01:40:03 +0530

    Add suggestions, remove bydefault keybindings and refine styling

--- src/pixelCLI.py
+++ src/pixelCLI.py
@@ -4,6 +4,7 @@ from textual.app import App, ComposeResult
 from textual.widgets import Button, Digits, Static, Input, LoadingIndicator, OptionList, Label
 from textual.containers import HorizontalGroup, VerticalScroll, Container
 import typer
+from textual.binding import Binding
 from rich.console import Console, ConsoleOptions, RenderResult
 
 console = Console()
@@ -12,12 +13,13 @@ class PixelatedCLI(App):
     """A basic CLI for Pixelated AI"""
 
     CSS_PATH = "Pixelated.tcss"
+    ENABLE_COMMAND_PALETTE = False
 
+    # TODO need to add more bindings like ctrl d
     BINDINGS = [
-        ("ctrl+c", "quit", "Quit")]
+        Binding("ctrl+c", "quit", "Exit Pixelated AI", priority=True)]
 
     def compose(self) -> ComposeResult:
-        """Create child widgets for the app."""
         with VerticalScroll(id="cli-corners"): # for corner color of cli and all vertical contents
             for i in range(50):
                 yield Static(f"Example {i+1} of scroll working")
@@ -26,6 +28,30 @@ class PixelatedCLI(App):
         with HorizontalGroup(id="input-container"):
             yield Label("> ", id="prompt-label")
             yield Input(placeholder="Imagine like you have never before...", id="user-input")
+
+        yield OptionList(
+            "clear - Start a new session",
+            "exit - Quit Pixelated AI",
+            id="menuCmds",
+            classes="hidden"
+        )
+
+    def on_input_changed(self, event: Input.Changed) -> None:
+        menuCmds = self.query_one("#menuCmds", OptionList)
+
+        if event.value.startswith("/"):
+            menuCmds.remove_class("hidden")
+        else:
+            menuCmds.add_class("hidden")
+
+    def on_option_list_option_selected(self, event: OptionList.OptionSelected) -> None:
+        inpWidget = self.query_one("#user-input", Input)
+        command = str(event.option.prompt).split(" - ")[0]
+        inpWidget.value = f"/{command} "
+        inpWidget.focus()
+
+        self.query_one("#menuCmds", OptionList).add_class("hidden")
+
 
 if __name__ == "__main__":
     app = PixelatedCLI()
