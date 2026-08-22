1d4735835f792fdb1445c8bfaee1065f028ce2bc
SURYANSH ROY <anirudroy.32@gmail.com>
2026-08-22 11:46:58 +0530

    good unhuman

--- src/pixel_logo.py
+++ src/pixel_logo.py
@@ -1,46 +1,40 @@
 from textual.widgets import Static
 from rich.text import Text
+from rich.color import Color
 from rich.panel import Panel
+import random
 
 UNHUMAN = """
          ░░░░░░░░   
         ▒▒▒▒▒▒▒▒▒▒  
-        ▆▆▆▆▆▆▆▆▆▆  
         ▌  ◝   ◜  ▌ 
        ╭▌  ⊙   ⊙  ▌╮
        ╰▌    !    ▌╯
         ▌  ╰===╯  ▌ 
-        ▌         ▌ 
       █▀▀▀▀▀▀▀▀▀▀▀▀▀█
 ミ▄▄▄▄█             █▄▄▄▄彡
       █             █
       █             █
-      █             █
-      █             █
-      █             █
       █▀▀▀▀▀▀▀▀▀▀▀▀▀█
       █             █
       █             █
-      █      |      █
-      █      |      █
-      █      |      █
-      █      |      █
-      █      |      █
       █      |      █
       █      |      █
       █     ▟ ▙     █
       █▄▄▄▄▄█ █▄▄▄▄▄█
 """
 
+rich_color = Color.from_rgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
 class Logo(Static):
     def render(self):
         logo_text = Text(UNHUMAN, justify="left")
-        logo_text.stylize("bold dim red")
+        logo_text.stylize(style=rich_color.name)
         
         return Panel(
             logo_text,
-            title="[bold red]Pixelated AI[/]",
-            border_style="cyan",
+            title=f"[bold color({random.randint(1, 9)})]Pixelated AI[/]",
+            border_style=f"bold color({random.randint(1, 5)})",
             expand=False,
             padding=(0, 2)
         )
